f5818e3a6a1fc55d965166c5e57a94d545eed1c8
Artem Chubenko <artem.chubenko@gmail.com>
2022-07-21 22:15:32 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,12 +7,6 @@ import Modal from './Modal';
 
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 
-const PIXA_KEY = '28226957-200d43869ee80bd5ab4812e4f';
-const URL = `https://pixabay.com/api/?key=${PIXA_KEY}`;
-
-let page = 1;
-let perPage = 10;
-
 export class App extends Component {
   state = {
     searchterm: '',
